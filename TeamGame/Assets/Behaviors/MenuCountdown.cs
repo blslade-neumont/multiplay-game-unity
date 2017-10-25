1d9036cfb800b77eb117d6b1a8df9ba9d8dbcf92
Brandon <brandonyoyoslade@gmail.com>
2017-10-25 09:53:25 -0600

    feat(menu): add ready countdown to begin the game

---
+++ TeamGame/Assets/Behaviors/MenuCountdown.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class MenuCountdown : MonoBehaviour
+{
+    [SerializeField] private string[] _readyKeys;
+    [SerializeField] private int _countdown = 3;
+
+    private Text _countdownText;
+    private float _currentCountdown = 3;
+
+    void Start()
+    {
+        _countdownText = GetComponent<Text>();
+        if (_readyKeys == null || _readyKeys.Length == 0)
+        {
+            _readyKeys = new[] { "Fire1", "Fire2" };
+        }
+    }
+    
+    void Update()
+    {
+        var allReadyKeysDown = true;
+        foreach (var key in _readyKeys)
+        {
+            if (!Input.GetButton(key))
+            {
+                allReadyKeysDown = false;
+                _currentCountdown = _countdown;
+                break;
+            }
+        }
+
+        _countdownText.text = "" + Mathf.Ceil(_currentCountdown);
+        _countdownText.enabled = allReadyKeysDown;
+
+        _currentCountdown -= Time.deltaTime;
+        if (_currentCountdown <= 0)
+        {
+            startGame();
+        }
+    }
+
+    private void startGame()
+    {
+        var controller = new GameController();
+        controller.StartGame();
+    }
+}
---
+++ TeamGame/Assets/Scripts/GameController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine.SceneManagement;
+
+public class GameController
+{
+    public GameController()
+    {
+        
+    }
+
+    private bool isStarted = false;
+    public void StartGame()
+    {
+        if (isStarted) throw new InvalidOperationException("This game has already been started!");
+        isStarted = true;
+        loadMinigame();
+    }
+    public void EndMinigame(int p1score, int p2score)
+    {
+        p1score += p1score;
+        p2score += p2score;
+        loadMinigame();
+    }
+
+    private int p1score = 0, p2score = 0;
+    private int minigamesLeft = 10;
+    
+    private void loadMinigame()
+    {
+        if (this.minigamesLeft-- == 0)
+        {
+            endGame();
+            return;
+        }
+
+        var rnd = new Random();
+        switch (rnd.Next(3))
+        {
+        case 0:
+            SceneManager.LoadScene("Skyscrapers");
+            break;
+
+        case 1:
+            SceneManager.LoadScene("Lasers");
+            break;
+
+        case 2:
+            SceneManager.LoadScene("FlappyBirds");
+            break;
+
+        default:
+            throw new InvalidOperationException();
+        }
+    }
+    private void endGame()
+    {
+        SceneManager.LoadScene("GameEnd");
+    }
+}
