e6be72b9de75c921aa340ec63d958b44aab17e07
ksHDO <hiddendeathorb@gmail.com>
2017-10-25 09:17:24 -0600

    Added _readyColor & _notReadyColor to ReadyState.cs

--- TeamGame/Assets/Scripts/ReadyState.cs
+++ TeamGame/Assets/Scripts/ReadyState.cs
@@ -10,6 +10,9 @@ public class ReadyState : MonoBehaviour
     [SerializeField] private string _ready;
     [SerializeField] private string _notReady;
 
+    [SerializeField] private Color _readyColor = new Color(0f, 0.8f, 0f);
+    [SerializeField] private Color _notReadyColor = new Color(0.8f, 0.0f, 0f);
+
     [SerializeField] private string _readyButton;
 
     private Text _text;
@@ -23,6 +26,10 @@ public class ReadyState : MonoBehaviour
 	// Update is called once per frame
 	void Update()
 	{
-	    _text.text = Input.GetButton(_readyButton) ? _ready : _notReady;
+	    bool ready = Input.GetButton(_readyButton);
+
+        _text.text = ready ? _ready : _notReady;
+	    _text.color = ready ? _readyColor : _notReadyColor;
+
 	}
 }
