45991e8cc2a9150a40e755e58672a3f47387a9ca
ksHDO <hiddendeathorb@gmail.com>
2017-10-27 09:34:49 -0600

    Laser.unity progress - Animator on players - Working on lasers - Added LaserPlayerFire.cs

---
+++ TeamGame/Assets/Scenes/Laser/Scripts/LaserPlayerFire.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[AddComponentMenu("Player/Laser Player Fire")]
+[RequireComponent(typeof(Animator))]
+public class LaserPlayerFire : MonoBehaviour
+{
+
+    [SerializeField] private string _fireTrigger;
+    [SerializeField] private string _fireButton;
+    [SerializeField] private ShakeObject _cameraShakey;
+
+    private Animator _animator;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    _animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    bool isFiring = Input.GetButton(_fireButton);
+        _animator.SetBool(_fireTrigger, isFiring);
+        if (isFiring)
+            _cameraShakey.Shake();
+
+
+    }
+}
