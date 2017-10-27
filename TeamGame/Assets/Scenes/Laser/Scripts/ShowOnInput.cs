838200f78488c460a50cf4e02952d76decd61a8f
ksHDO <hiddendeathorb@gmail.com>
2017-10-27 09:53:02 -0600

    Laser.unity progress

---
+++ TeamGame/Assets/Scenes/Laser/Scripts/ShowOnInput.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowOnInput : MonoBehaviour
+{
+
+    [SerializeField] private string _inputKey;
+    [SerializeField] private bool _invert = false;
+    [SerializeField] private GameObject _toShow;
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+	    if (_toShow)
+	    {
+	        _toShow.SetActive(Input.GetButton(_inputKey) != _invert);
+	    }
+	}
+}
